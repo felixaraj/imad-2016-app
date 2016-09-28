b70b6b0492a06f85dab827515df24516ba9b9f22
Felix <felixaraj2015@gmail.com>
2016-09-28 19:32:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res) {
-  res.send('Article-three which is requested will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
