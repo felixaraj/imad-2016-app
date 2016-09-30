af0db1bc748d50648d3aade559d3f21d9299fe4a
Felix <felixaraj2015@gmail.com>
2016-09-30 15:28:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,6 +120,11 @@ app.get('/ui/main.js', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.tostring());
+});
+
 app.get('/ui/png-sunflower.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'png-sunflower.png'));
 });
