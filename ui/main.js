ca623cde7c2004a8045d215b05ae25d7369575c4
Felix <felixaraj2015@gmail.com>
2016-09-30 12:18:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,6 @@
+// change the text of the main-text div
 
+var element = document.getElementById('main-text');
+element.innerHTML = 'Love & Peace to all';
 
 console.log('Felix Arokiya Raj! God Bless You all');
