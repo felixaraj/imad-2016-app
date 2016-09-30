4fdf4e64f71c7a038ec815e04185b9452e93809a
Felix <felixaraj2015@gmail.com>
2016-09-30 12:52:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,17 @@ element.innerHTML = 'Love & Peace to all';
 // moving the image
 
 var img = document.getElementById('oliver');
+
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50);  
+};
+
 img.onclick = function() {
     img.style.marginLeft = '100px';
 };
