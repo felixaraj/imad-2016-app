84f36dac6a855624d222224a46f49784deeca9cf
Felix <felixaraj2015@gmail.com>
2016-09-30 12:29:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,11 @@
 var element = document.getElementById('main-text');
 element.innerHTML = 'Love & Peace to all';
 
+// moving the image
+
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
+
 console.log('Felix Arokiya Raj! God Bless You all');
